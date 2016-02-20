f7f8c2558d4973723f93d77c97f012b97314100c
denis <unitrino@rambler.ru>
2016-02-20 15:19:11 +0200

    First

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,26 +10,6 @@ import play.api.db._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index(null))
-  }
-
-  def db = Action {
-    var out = ""
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-    } finally {
-      conn.close()
-    }
-    Ok(out)
+    Ok(views.html.index())
   }
 }
