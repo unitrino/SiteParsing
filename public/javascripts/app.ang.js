c77809be5b1c29d467d27e8bbfd66d799a90d726
denis <unitrino@rambler.ru>
2016-09-18 13:00:10 +0300

    Alpha verions without makeup

--- public/javascripts/app.ang.js
+++ public/javascripts/app.ang.js
@@ -6,6 +6,8 @@ var main_app = angular.module('main-app', []);
 main_app.controller('AppCtrl', function($scope, $http) {
 
     $scope.errors = "";
+    $scope.link_generate = false;
+
     $scope.testing_data = function(site_url, html_tags) {
         $http({
             method: 'POST',
@@ -14,10 +16,13 @@ main_app.controller('AppCtrl', function($scope, $http) {
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).success(function(answ){
             console.log(answ);
+            $scope.errors = "";
+            $scope.link_generate = false;
             $scope.tesing_answ = answ;
         })
         .error(function(answ){
             console.log(answ);
+            $scope.link_generate = false;
             $scope.errors = answ;
         });
 
@@ -31,10 +36,13 @@ main_app.controller('AppCtrl', function($scope, $http) {
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).success(function(answ){
             console.log(answ);
+            $scope.errors = "";
+            $scope.link_generate = true;
             $scope.link_of = answ;
         })
         .error(function(answ){
             console.log(answ);
+            $scope.link_generate = false;
             $scope.errors = answ;
         });
 
